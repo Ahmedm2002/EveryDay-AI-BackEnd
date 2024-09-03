6807c1a12cbf2daba106cf71798758206bfe1ba6
Ahmedm2002 <ahmedmujtaba0159@gmail.com>
2024-09-03 22:09:02 +0500

    Maked the sockets working --Ahmed

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
 const express = require('express');
 const { createServer } = require('node:http')
-const  path  = require('node:path')
 const { Server } = require('socket.io');
-
+const cors = require("cors");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+require('dotenv').config();
 
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
 
-const genAI = new GoogleGenerativeAI('AIzaSyCWD8R-irLSB8wFkiLR8d5NkA3xqZVYdQg');
+const genAI = new GoogleGenerativeAI(process.env.APIKey);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(cors(corsOptions));
+
 
 const server = createServer(app)
 
-const io = new Server(server);
+
+const io = new Server(server, {
+  cors:{
+    origin: "*",
+    optionsSuccessStatus: 200,
+  }
+})
+
 
 io.on('connection', (socket) =>{
 
   socket.on('userMsg', async(message)=>{
+    
+    console.log(`UserMsg: ${message}`);
+    
+
     try {
+    
       const botResult = await model.generateContent(message);
+    
       const result = botResult.response.text();
+    
+      console.log(`Result: ${result}`);
+      
+
       io.emit("result", result);
+    
     } catch (error) {
+    
+      console.log(`Error: ${error}`);
+
+
       io.emit("result", error.message)
+    
     }
   })
 })
-
-
-
-app.get('/', async(req, res) => {
-  try {
-    res.sendFile(path.join(__dirname, 'index.html'));  
-  } catch (error) {
-    res.status(400).json({error: error.message});
-  }
-});
-
 
 
 const port = 3000;
